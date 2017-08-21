4f5627d167ba118445ab0492880700f477b89e93
Ronaldo T. Duarte <ronaldo.tomazeli@gmail.com>
2017-08-21 17:48:43 -0300

    playing with CPython

--- cpython/grupymodule.c
+++ cpython/grupymodule.c
@@ -2,20 +2,22 @@
 // Python.h já inclui alguns .h padrão (stdio, string, errno, stdlib)
 #include <Python.h>
 
-static PyObject* grupy_sum(PyObject* self, PyObject* args);
-static PyObject* grupy_sum_tuple(PyObject* self, PyObject* args);
+static PyObject* sum(PyObject* self, PyObject* args);
+static PyObject* sum_tuple(PyObject* self, PyObject* args);
+
+static PyObject* list_new(PyTypeObject* type, PyObject* args, PyObject* kwargs);
 
 typedef struct {
     PyObject_HEAD
     long* head;
     long* tail;
     long* cur;
-} grupy_list;
+} list;
 
-static PyTypeObject grupy_list_type = {
+static PyTypeObject list_type = {
     PyVarObject_HEAD_INIT(NULL, 0)
     "grupy.list",               /* tp_name */
-    sizeof(grupy_list),         /* tp_basicsize */
+    sizeof(list),               /* tp_basicsize */
     0,                          /* tp_itemsize */
     0,                          /* tp_dealloc */
     0,                          /* tp_print */
@@ -33,44 +35,60 @@ static PyTypeObject grupy_list_type = {
     0,                          /* tp_setattro */
     0,                          /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT,         /* tp_flags */
-    "grupy awesome list"        /* tp_doc */
+    "grupy awesome list",       /* tp_doc */
+    0,                          /* tp_traverse */
+    0,                          /* tp_clear */
+    0,                          /* tp_richcompare */
+    0,                          /* tp_weaklistoffset */
+    0,                          /* tp_iter */
+    0,                          /* tp_iternext */
+    0,                          /* tp_methods */
+    0,                          /* tp_members */
+    0,                          /* tp_base */
+    0,                          /* tp_dict */
+    0,                          /* tp_descr_get */
+    0,                          /* tp_descr_set */
+    0,                          /* tp_dictoffset */
+    0,                          /* tp_init */
+    0,                          /* tp_alloc */
+    list_new,                   /* tp_new */
 };
 
-static PyMethodDef grupy_methods[] = {
-    { "sum", grupy_sum, METH_VARARGS, "Calcula a soma de dois inteiros." },
-    { "sum_tuple", grupy_sum_tuple, METH_VARARGS, "Calcula a soma dos números em uma tupla." },
+static PyMethodDef methods[] = {
+    { "sum", sum, METH_VARARGS, "Calcula a soma de dois inteiros." },
+    { "sum_tuple", sum_tuple, METH_VARARGS, "Calcula a soma dos números em uma tupla." },
     { NULL, NULL, 0, NULL }
 };
 
-static struct PyModuleDef grupy_module = {
+static struct PyModuleDef module = {
     PyModuleDef_HEAD_INIT,
     "grupy",
     "Doc for grupy module.",
     -1,
-    grupy_methods
+    methods
 };
 
 PyMODINIT_FUNC PyInit_grupy(void) {
     PyObject* m;
 
-    grupy_list_type.tp_new = PyType_GenericNew;
+    list_type.tp_new = PyType_GenericNew;
 
-    if (PyType_Ready(&grupy_list_type) < 0) {
+    if (PyType_Ready(&list_type) < 0) {
         return NULL;
     }
 
-    m = PyModule_Create(&grupy_module);
+    m = PyModule_Create(&module);
     if (!m) {
         return NULL;
     }
 
-    Py_INCREF(&grupy_list_type);
-    PyModule_AddObject(m, "list", (PyObject*)&grupy_list_type);
+    Py_INCREF(&list_type);
+    PyModule_AddObject(m, "list", (PyObject*)&list_type);
 
     return m;
 }
 
-static PyObject* grupy_sum(PyObject* self, PyObject* args) {
+static PyObject* sum(PyObject* self, PyObject* args) {
     int x, y;
 
     if (!PyArg_ParseTuple(args, "ii", &x, &y)) {
@@ -80,7 +98,7 @@ static PyObject* grupy_sum(PyObject* self, PyObject* args) {
     return Py_BuildValue("i", x + y);
 }
 
-static PyObject* grupy_sum_tuple(PyObject* self, PyObject* args) {
+static PyObject* sum_tuple(PyObject* self, PyObject* args) {
     PyObject* tuple;
 
     if (!PyArg_ParseTuple(args, "O", &tuple)) {
